3b66481580ef898ae51ff81bacebad4d716ff242
LayNath242 <nath.selendra@gmail.com>
2022-10-28 14:08:23 +0700

    add indranet runtime

--- runtimes/indranet/build.rs
+++ runtimes/indranet/build.rs
@@ -14,21 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#![cfg_attr(not(feature = "std"), no_std)]
+use substrate_wasm_builder::WasmBuilder;
 
-use frame_support::weights::{constants::WEIGHT_PER_SECOND, Weight};
-
-use sp_runtime::Perbill;
-
-pub mod impls;
-pub mod xcm_config;
-
-/// We assume that ~5% of the block weight is consumed by `on_initialize` handlers. This is
-/// used to limit the maximal weight of a single extrinsic.
-pub const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(5);
-/// We allow `Normal` extrinsics to fill up the block up to 75%, the rest can be used by
-/// Operational  extrinsics.
-pub const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
-
-/// We allow for 0.5 seconds of compute with a 6 second average block time.
-pub const MAXIMUM_BLOCK_WEIGHT: Weight = WEIGHT_PER_SECOND / 2;
+fn main() {
+	WasmBuilder::new()
+		.with_current_project()
+		.export_heap_base()
+		.import_memory()
+		.build()
+}
